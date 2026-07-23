aae0ea419ec40bb675992c8a48dbf805c640d40b
Alexander Kolodka <alexander@kolodka.fyi>
2026-07-24 02:22:07 +0700

    refactor: cron usecase

--- internal/cases/cron/handler.go
+++ internal/cases/cron/handler.go
@@ -1,8 +1,9 @@
-package runpipelines
+package cron
 
 import (
 	"context"
 
+	enginecron "github.com/alexander-kolodka/crestic/internal/engine/cron"
 	"github.com/alexander-kolodka/crestic/internal/engine/jobs"
 	"github.com/alexander-kolodka/crestic/internal/engine/pipelines"
 	"github.com/alexander-kolodka/crestic/internal/entity"
@@ -10,6 +11,7 @@ import (
 
 type Command struct {
 	Pipelines   []entity.Pipeline
+	StateFile   string
 	DryRun      bool
 	Healthcheck bool
 }
@@ -18,6 +20,7 @@ type Handler struct {
 	pipelines *pipelines.Runner
 }
 
+// NewHandler creates a cron command Handler.
 func NewHandler(pipelinesRunner *pipelines.Runner) *Handler {
 	return &Handler{
 		pipelines: pipelinesRunner,
@@ -25,9 +28,18 @@ func NewHandler(pipelinesRunner *pipelines.Runner) *Handler {
 }
 
 func (h *Handler) Handle(ctx context.Context, cmd *Command) error {
+	due, err := enginecron.FilterPipelinesByCron(ctx, cmd.Pipelines, cmd.StateFile)
+	if err != nil {
+		return err
+	}
+
+	if len(due) == 0 {
+		return nil
+	}
+
 	if cmd.DryRun {
 		ctx = jobs.WithDryRun(ctx)
 	}
 
-	return h.pipelines.Run(ctx, cmd.Pipelines, cmd.Healthcheck)
+	return h.pipelines.Run(ctx, due, cmd.Healthcheck)
 }
